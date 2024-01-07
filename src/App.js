d8bcdd9c36b637fb94343262a3faf31fd8942796
Cwdrian <xcharlesdrian@gmail.com>
2024-01-07 14:26:47 -0500

    login and regs forms

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login')
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
+      }
     </div>
   );
 }
