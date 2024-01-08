6d36f5536be9959a3e7d018d7658a355ee632e22
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2024-01-08 18:19:20 -0500

    initial Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Navigation from "./Components/Navigation";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      Testing
     </div>
   );
 }
---
+++ src/Components/Navigation.js
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+import Nav from 'react-bootstrap/Nav';
+
+function Navigation() {
+    return (
+        <Navbar className="bg-body-tertiary, justify-content-start">
+            <Container>
+                <Navbar.Brand href="/">Rock-n-Reviews</Navbar.Brand>
+                    <Nav className="justify-content-end" activeKey="/home">
+                    <Nav.Item>
+                        <Nav.Link href="/Forum">Forum</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/Login_Register">Login/Register</Nav.Link>
+                    </Nav.Item>
+                    </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Navigation;
