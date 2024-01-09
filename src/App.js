f0f13c2e71309eb3a7f2f564d7978e9bff8b8dc3
NeNeZoRr <zorrillalin@gmail.com>
2024-01-08 20:39:22 -0500

    test-2

--- src/App.js
+++ src/App.js
@@ -1,87 +1,51 @@
+import React, { useEffect, useState } from "react";
 import Navigation from "./Components/Navigation";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import './App.css';
 import Search from "./Components/Search";
-import { useEffect, useState } from "react";
 import Home from "./Components/Home";
-import Navigation from "./Components/Navigation";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Forum from './Components/Forum';
-import Navigation from "./Components/Navigation";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import Search from "./Components/Search";
-import { useEffect, useState } from "react";
-import Home from "./Components/Home";
-import Navigation from "./Components/Navigation";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Forum from './Components/Forum';
 
 function App() {
-    let [search, setSearch] = useState("");
-    let [message, setMessage] = useState("search for music");
-    let [data, setData] = useState([]);
-  
-    //const API_URL = "https://itunes.apple.com/search?term=";
-  
-    useEffect(() => {
-      if (search) {
-        const fetchData = async () => {
+  const [search, setSearch] = useState("");
+  const [message, setMessage] = useState("Search for music");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (search) {
+      const fetchData = async () => {
+        try {
           const uri = encodeURI(`https://itunes.apple.com/search?term=${search}`);
           const response = await fetch(uri);
           const resData = await response.json();
-          console.log(resData);
+
           if (resData.results.length > 0) {
             setData(resData.results);
           } else {
-            setMessage("not Found");
+            setMessage("Not Found");
           }
-        };
-        fetchData();
-      }
-    }, [search]);
-  
-    const handleSearch = (e, term) => {
-      e.preventDefault();
-      setSearch(term);
-    };
-    return (
-    let [search, setSearch] = useState("");
-    let [message, setMessage] = useState("search for music");
-    let [data, setData] = useState([]);
-  
-    //const API_URL = "https://itunes.apple.com/search?term=";
-  
-    useEffect(() => {
-      if (search) {
-        const fetchData = async () => {
-          const uri = encodeURI(`https://itunes.apple.com/search?term=${search}`);
-          const response = await fetch(uri);
-          const resData = await response.json();
-          console.log(resData);
-          if (resData.results.length > 0) {
-            setData(resData.results);
-          } else {
-            setMessage("not Found");
-          }
-        };
-        fetchData();
-      }
-    }, [search]);
-  
-    const handleSearch = (e, term) => {
-      e.preventDefault();
-      setSearch(term);
-    };
-    return (
-      <div className="App">
-        <Navigation />
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+
+      fetchData();
+    }
+  }, [search]);
+
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    setSearch(term);
+  };
+
+  return (
+    <div className="App">
       <Navigation />
-      Testing
-      <Forum/>
+      {/* Testing */}
+      <Forum />
       <Home />
-      </div>
-    );
-    );
+    </div>
+  );
+}
 
 export default App;
