c62b8cfddbe67fb550128ee858ee138791a41c26
Cwdrian <xcharlesdrian@gmail.com>
2024-01-08 20:28:11 -0500

    added password fields

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export const Login = (props) => {
     const [user, setUser] = useState('');
+    const [pass, setPass] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -13,6 +14,8 @@ export const Login = (props) => {
         <form onSubmit={handleSubmit}>
             <label for="user">User Name:</label>
             <input value={user} onChange={(e) => setUser(e.target.value)} type="user Name" placeholder="User Name"/>
+            <label for="pass">Password:</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********"/>
             <button type="submit">Log In</button>
         </form>
         <button onClick={() => props.onFormSwitch('register')}> Need to register?</button>
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 export const Register = (props) => {
     const [user, setUser] = useState('');
     const [name, setName] = useState('');
+    const [pass, setPass] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -17,6 +18,8 @@ export const Register = (props) => {
             <label for="user">User Name:</label>
             <input value={user} onChange={(e) => setUser(e.target.value)} type="user Name" placeholder="User Name"/>
             <button type="submit">Register</button>
+            <label for="pass">Password:</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********"/>
         </form>
         <button onClick={() => props.onFormSwitch('login')}>Already have an account?</button>
         </>
