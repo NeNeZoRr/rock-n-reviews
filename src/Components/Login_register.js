22e15a81d1842848fe685ef4b97091f99570d597
tamccullough88 <tamccullough88@gmail.com>
2024-01-10 20:55:33 -0500

    fixing the logreg page code

--- src/Components/Login_register.js
+++ src/Components/Login_register.js
@@ -1,21 +1,14 @@
-import Login from "./Login";
-import Register from "./Register";
-import { useState } from 'react'
-
-function Logreg() {
+const Logreg = () => {
     const [currentForm, setCurrentForm] = useState('login')
 
     const toggleForm = (formName) => {
         setCurrentForm(formName);
     }
-
     return (
-        <div >
-            {
-                currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-            }
-        </div>
-
-    )
+        <>
+            Login
+        </>
+    );
 }
+
 export default Logreg;
