5722f16e5e67e572a3f6320f765144cf0b74ba1c
Cwdrian <xcharlesdrian@gmail.com>
2024-01-18 14:12:56 -0500

    trying

--- src/Components/Login_register.js
+++ src/Components/Login_register.js
@@ -1,22 +1,9 @@
-// import { useState } from "react";
-// import '../App.css';
-// import Login from "./Login";
-// import { Register } from "./Register";
+const Logreg = () => {
+    return (  
+        <>
+        Login
+        </>
+    );
+}
 
-
-// const Logreg = () => {
-//     const [currentForm, setCurrentForm] = useState('login')
-
-//     const toggleForm = (formName) => {
-//         setCurrentForm(formName);
-//     }
-//     return (  
-//         <div>
-//         {
-//             currentForm === 'login'? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-//         }
-//         </div>
-//     );
-// }
-
-// export default Logreg;
+export default Logreg;
